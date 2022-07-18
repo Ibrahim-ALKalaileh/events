6fd7268485b4a04e5a874c2d772f5ae1b141e2cd
Ibrahim-AlKalaileh <Ibrahim.al.kalaileh@gmail.com>
2022-07-18 15:17:01 +0300

    created the newmeetups page and form and connected them to fireBase

--- src/pages/NewMeetUps.js
+++ src/pages/NewMeetUps.js
@@ -1,5 +1,24 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
-    return <div>New MeetupPage</div>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://meetup-project-react-4a841-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
-  
-  export default NewMeetupPage;
+
+  return (
+    <div>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </div>
+  );
+}
+
+export default NewMeetupPage;
